00afee09982451253173d36f1ef24379115a6f31
Jess Yuan <zhuohao.yuen@gmail.com>
2020-02-25 17:42:47 +0800

    Update run-apt.sh

--- run-apt.sh
+++ run-apt.sh
@@ -21,7 +21,7 @@ docker rm -f $(docker ps -a -q)
 #docker run -dt --name ss4 -p 6666:6666 mritd/shadowsocks -s "-s 0.0.0.0 -p 6666 -m aes-256-cfb -k 11223344 --fast-open"
 #docker run -dt --name ss5 -p 7777:7777 mritd/shadowsocks -s "-s 0.0.0.0 -p 7777 -m aes-256-cfb -k 11223344 --fast-open"
 bash <(curl -s -L https://git.io/v2ray.sh)
-curl -O https://raw.githubusercontent.com/atrandys/trojan/master/trojan_mult.sh && chmod +x trojan_mult.sh && ./trojan_mult.sh #trojan
-wget -N --no-check-certificate "https://raw.githubusercontent.com/chiakge/Linux-NetSpeed/master/tcp.sh" #bbr
+#curl -O https://raw.githubusercontent.com/atrandys/trojan/master/trojan_mult.sh && chmod +x trojan_mult.sh && ./trojan_mult.sh #安装trojan
+wget -N --no-check-certificate "https://raw.githubusercontent.com/chiakge/Linux-NetSpeed/master/tcp.sh" #bbr加速
 chmod +x tcp.sh
 ./tcp.sh
